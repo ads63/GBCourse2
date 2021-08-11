7e919294786ce061029f28933b9746d5b339796d
ads63 <ads63@yandex.ru>
2021-08-11 16:42:54 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Алексей Шинкарев on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
